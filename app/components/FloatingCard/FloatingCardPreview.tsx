c5429b24b1cd5d8ceeecd03c35db864a12feeb4e
Edmundas Ravdo <edmundas.ravdo@telesoftas.com>
2024-11-12 23:41:47 +0200

    Added CV

--- app/components/FloatingCard/FloatingCardPreview.tsx
+++ app/components/FloatingCard/FloatingCardPreview.tsx
@@ -7,10 +7,7 @@ import Link from 'next/link';
 export function FloatingCardPreview() {
   return (
     <CardContainer className='inter-var'>
-      <CardBody
-        className='group/card relative flex h-auto w-auto flex-col items-center rounded-xl border border-black/[0.1] bg-gray-100
-       p-6 hover:shadow-2xl hover:shadow-cyan-500/[0.1] sm:w-[30rem] dark:border-white/[0.2] dark:bg-black'
-      >
+      <CardBody className='group/card relative flex h-auto w-auto flex-col items-center rounded-xl border border-black/[0.1] bg-gray-100 p-6 hover:shadow-2xl hover:shadow-cyan-500/[0.1] sm:w-[30rem] dark:border-white/[0.2] dark:bg-black'>
         <CardItem
           translateZ='60'
           className='mt-4 text-xl font-bold text-neutral-950 dark:text-white'
@@ -24,7 +21,7 @@ export function FloatingCardPreview() {
         >
           Front-End Engineer
         </CardItem>
-        <div className='mt-14 flex items-center justify-between'>
+        <div className='mt-8 flex items-center justify-between'>
           <CardItem
             translateZ={60}
             as={Link}
@@ -40,6 +37,23 @@ export function FloatingCardPreview() {
           as='ul'
           className='flex gap-5 text-neutral-950 dark:text-white'
         >
+          <li className={'flex items-center'}>
+            <Link
+              href='/CV_Edmundas_Ravdo.pdf'
+              target='__blank'
+              className={'block h-[42px] w-[42px]'}
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill-rule='evenodd'
+                clip-rule='evenodd'
+                className='fill-current'
+                viewBox='0 0 24 24'
+              >
+                <path d='M3 24h19v-23h-1v22h-18v1zm17-24h-18v22h18v-22zm-1 1h-16v20h16v-20zm-2 16h-12v1h12v-1zm0-3h-12v1h12v-1zm0-3h-12v1h12v-1zm-7.348-3.863l.948.3c-.145.529-.387.922-.725 1.178-.338.257-.767.385-1.287.385-.643 0-1.171-.22-1.585-.659-.414-.439-.621-1.04-.621-1.802 0-.806.208-1.432.624-1.878.416-.446.963-.669 1.642-.669.592 0 1.073.175 1.443.525.221.207.386.505.496.892l-.968.231c-.057-.251-.177-.449-.358-.594-.182-.146-.403-.218-.663-.218-.359 0-.65.129-.874.386-.223.258-.335.675-.335 1.252 0 .613.11 1.049.331 1.308.22.26.506.39.858.39.26 0 .484-.082.671-.248.187-.165.322-.425.403-.779zm3.023 1.78l-1.731-4.842h1.06l1.226 3.584 1.186-3.584h1.037l-1.734 4.842h-1.044z' />
+              </svg>
+            </Link>
+          </li>
           <li>
             <Link
               href='https://www.linkedin.com/in/edmundas-ravdo'
